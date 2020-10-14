0e983d4a621a3e7a13f2e04a305a3434cc16eaaf
nyulin <aliny.beltramin@gmail.com>
2020-10-14 17:38:44 -0300

    Aula 01

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import mapMarkerImg from '../images/map-marker.svg';
+
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import '../styles/pages/orphanages-map.css';
 
 function OrphanagesMap(){
     return(
-        <h1>Hi.</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+                    <h2>Escolha um orfanato no mapa.</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Curitiba</strong>
+                    <span>Paraná</span>
+                </footer>
+            </aside>
+            <Map 
+                center={[-25.4321781,-49.2796458]}
+                zoom={15}
+                style={ { width: "100%", height: "100%" } }
+            >
+                <TileLayer 
+                    //url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                />
+            </Map>
+            <Link to="" className="create-orphanage" >
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
 
